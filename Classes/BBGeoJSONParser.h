77a1574a6fa328cbc1d6b7d9f360f02f57ef3c55
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2012-10-02 18:04:43 -0400

    Move Tile classes under the class folder

---
+++ Classes/BBGeoJSONParser.h
@@ -0,0 +1,13 @@
+//
+//  BBGeoJSONParser.h
+//  map
+//
+//  Created by Ben on 10/2/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBGeoJSONParser : NSObject
+
+@end
---
+++ Classes/BBGeoJSONParser.m
@@ -0,0 +1,13 @@
+//
+//  BBGeoJSONParser.m
+//  map
+//
+//  Created by Ben on 10/2/12.
+//
+//
+
+#import "BBGeoJSONParser.h"
+
+@implementation BBGeoJSONParser
+
+@end
